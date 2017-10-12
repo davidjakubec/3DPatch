57710f6dfd361c6a7c44f1beb9d5212d1bb59b8f
David Jakubec <david.jakubec@gmail.com>
2017-10-12 11:28:12 +0100

    Input error handling.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+Set.prototype.intersection = function(setB) {
+    var intersection = new Set();
+    for (var elem of setB) {
+        if (this.has(elem)) {
+            intersection.add(elem);
+        }
+    }
+    return intersection;
+}
+
+/*----------------------------------------------------------------------------*/
+
 function phmmerRequest(seq, seqdb, callback) {
 //    var url = "https://www.ebi.ac.uk/Tools/hmmer/search/phmmer";
     var url = "http://ves-hx-b6.ebi.ac.uk/Tools/hmmer/search/phmmer";
@@ -33,7 +45,7 @@ function hmmsearchRequest(resultsURL, callback) {
     var jobID = resultsURL.split("/")[6];
 //    var url = "https://www.ebi.ac.uk/Tools/hmmer//search/hmmsearch?uuid=" + jobID + ".1";
     var url = "http://ves-hx-b6.ebi.ac.uk/Tools/hmmer//search/hmmsearch?uuid=" + jobID + ".1";
-//    console.log(url);
+    console.log(url);
     var data = new FormData();
     data.append("algo", "hmmsearch");
     data.append("seqdb", "pdb");
@@ -49,6 +61,8 @@ function hmmsearchCallback(request) {
         var responseURL = request.responseURL;
         console.log(responseURL);
         phmmerResultsHMMRequest(responseURL, phmmerResultsHMMCallback);
+        var hits = JSON.parse(request.response)["results"]["hits"];	// all hits, incl. high E-value
+        console.log(hits);
     }
 }
 
@@ -61,7 +75,7 @@ function phmmerResultsHMMRequest(resultsURL, callback) {
 }
 
 function phmmerResultsHMMCallback(request) {
-    var phmmerResultsHMM = request.response;
+    var phmmerResultsHMM = request.response;	// HMM from high-scoring (above threshold) phmmer search hits
 //    console.log(phmmerResultsHMM);
     var phmmerResultsHMMBlob = new Blob([phmmerResultsHMM], {type: "text/plain"});
     skylignRequest("hmm", phmmerResultsHMMBlob, skylignCallback);
@@ -86,10 +100,29 @@ function skylignCallback(request) {
     }
 }
 
+/*----------------------------------------------------------------------------*/
+
 document.querySelector("#submitSequenceButton").onclick = function() {
-    var sequence = document.querySelector("#sequenceInput").value.toUpperCase();
+    var sequence = document.querySelector("#sequenceInput").value.toUpperCase().replace(/\s+/g, "");
     console.log(sequence);
+    checkInputSequence(sequence);
     phmmerRequest(sequence, "uniprotrefprot", phmmerCallback);
 }
 
-// MERKRGRQTYTRYQTLELEKEFHFNRYLTRRRRIEIAHALSLTERQIKIWFQNRRMKWKKEN
+function checkInputSequence(seq) {
+    var seqLength = seq.length;
+    var seqUniqueCharacters = new Set(seq);
+    var nucleotides = new Set("ACGT");
+    if (seqLength < 10) {
+        throw new Error("Input sequence must contain at least 10 characters.");
+    }
+    if ((seqLength === 10) && (seqUniqueCharacters.size < 6)) {
+        throw new Error("At least 6 unique characters must be present in an input sequence containing exactly 10 characters.");
+    }
+    if ((seqLength > 10) && (seqUniqueCharacters.size === (seqUniqueCharacters.intersection(nucleotides)).size)) {
+        throw new Error("At least 1 character which is not A/C/G/T must be present in an input sequence containing more than 10 characters.");
+    }
+}
+
+// MERKRGRQTYTRYQTLELEKEFHFNRYLTRRRRIEIAHALSLTERQIKIWFQNRRMKWKKEN	9ant
+// EQACDICRLKKLKCSKEKPKCAKCLKNNWECRYSPKTKRSPLTRAHLTEVESRLERLEQLFLLIFPREDLDMILKMDSLQ	3coq
