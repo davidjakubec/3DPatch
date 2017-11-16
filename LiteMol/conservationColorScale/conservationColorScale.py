edf3f5cc4448f9061a64992c942433f75647d2f2
David Jakubec <david.jakubec@gmail.com>
2017-11-16 11:46:35 +0000

    Number of colors tweaking.

--- LiteMol/conservationColorScale/conservationColorScale.py
+++ LiteMol/conservationColorScale/conservationColorScale.py
@@ -1,4 +1,5 @@
 from matplotlib import cm
+from numpy import linspace
 
 cmap = []
 
@@ -8,5 +9,24 @@ for i in range(256):
 
 cmap.reverse()
 
+number_of_colors = 20
+boundaries = linspace(0, 255, number_of_colors + 1)
+color_ids = []
+
+for i in range(0, len(boundaries) - 1):
+    color_id = (boundaries[i] + boundaries[i + 1]) / 2
+    color_ids.append(int(round(color_id)))
+
+color_scale = []
+
+for color_id in color_ids:
+    for i in range(256 // number_of_colors):
+        color_scale.append(cmap[color_id])
+
+for i in range(256 % number_of_colors):
+    color_scale.append(cmap[color_ids[-1]])
+
+print(len(set([tuple(i) for i in color_scale])), len(color_scale))
+
 with open('conservationColorScale.js', encoding = 'UTF-8', mode = 'w') as file:
-    file.write('var conservationColorScale = ' + str(cmap) + ';')
+    file.write('var conservationColorScale = ' + str(color_scale) + ';')
